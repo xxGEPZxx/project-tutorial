6d5326b3c3ab2ee732f2f972873c0010575d0497
GEPZ <guillermop2507@gmail.com>
2023-12-10 18:06:38 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+// funcion de un contador y los muestra por consola
+
+ function contador(){
+    for (var i = 0; i < 10; i++) {
+        console.log(i);
+    }
+}
---
+++ main.js
@@ -0,0 +1,9 @@
+// Traer lo de index.js a main.js
+
+  
+// Importar la funcion contador
+import {contador} from './index.js';
+
+// Ejecutar la funcion contador
+
+contador();
